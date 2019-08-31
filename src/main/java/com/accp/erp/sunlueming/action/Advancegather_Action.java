3ed970fd453e5d8dae63017600c722f44501962f
SunLueMing <jioubidao@163.com>
2019-08-31 16:15:25 +0800

    2019.08.31 16:15

--- src/main/java/com/accp/erp/sunlueming/action/Advancegather_Action.java
+++ src/main/java/com/accp/erp/sunlueming/action/Advancegather_Action.java
@@ -2,10 +2,13 @@ package com.accp.erp.sunlueming.action;
 
 import com.accp.erp.sunlueming.biz.AdvancegatherBiz;
 import com.accp.erp.sunlueming.pojo.Advancegather;
+import com.accp.erp.sunlueming.util.CreateSingleNum;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -20,9 +23,23 @@ public class Advancegather_Action {
     @Resource
     private AdvancegatherBiz biz;
 
-    @RequestMapping("/sel/advancedetails")
-    public List<Advancegather> SellAll() {
-        return biz.SelAll();
+    CreateSingleNum createSingleNum = new CreateSingleNum();
+    /**
+     * @description: 查询预收款数据
+     * @author: apple
+     */
+    @RequestMapping("/sel/{biilid}")
+    public Advancegather SellAll(@PathVariable String biilid) {
+        return biz.SelSome(biilid);
     }
 
+    /**
+     * @description: 根据单据创建单号
+     * @author: apple
+     */
+    @RequestMapping("/single")
+    public String singleNum() {
+        String code = "YFK";
+        return createSingleNum.AddNum(code);
+    }
 }
