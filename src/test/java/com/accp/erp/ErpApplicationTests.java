f40859aa75a96e90664aaa60e46dd6a5877a9fef
= <=>
2019-09-03 16:40:36 +0800

    孙略铭20190903

--- src/test/java/com/accp/erp/ErpApplicationTests.java
+++ src/test/java/com/accp/erp/ErpApplicationTests.java
@@ -7,36 +7,14 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import com.accp.erp.yangtao.biz.ClientBiz;
-import com.accp.erp.yangtao.biz.ClienttypeBiz;
-import com.accp.erp.yangtao.biz.DepttabBiz;
-import com.accp.erp.yangtao.biz.EmpolyeeBiz;
-import com.accp.erp.yangtao.biz.GeneralledgerBiz;
-import com.accp.erp.yangtao.biz.MaterielBiz;
-import com.accp.erp.yangtao.biz.MaterieltypeBiz;
-import com.accp.erp.yangtao.biz.ProvideinfoBiz;
-import com.accp.erp.yangtao.biz.ProvidetypeBiz;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ErpApplicationTests {
-	@Autowired
-	private EmpolyeeBiz ebiz;
-	@Autowired
-	private ClienttypeBiz biz;
-	@Autowired
-	private ClientBiz Cbiz;
-	@Autowired
-	private DepttabBiz Abiz;
-	@Autowired
-	private ProvidetypeBiz Ebiz;
-	
 @Autowired
 private UserBiz userBiz;
     @Test
     public void contextLoads() {
-    	Ebiz.findStorage();
+
         User admin = userBiz.selectByPwdByAccount("admin", "202cb962ac59075b964b07152d234b70");
         System.out.println(admin);
     }
